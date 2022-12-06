6dc7bee6b270ec930a425f27ebad1da35bca0cdd
Victoria Shevchenko <victoria.shevchenko@ens.psl.eu>
2022-12-06 16:27:56 +0100

    alignment on BMRC

--- send_job_align.sh
+++ send_job_align.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
+
 #SBATCH --job-name=GradAlign
 #SBATCH -p short
+#$ -t 1-527:1
 
 # I want the job to take 3 gradients of all 900 subjects, align them to Margulies 2016 and compite ICC for each vertex abd value of neighbours. Returns a huge csv with results, a long dataframe
 module load Python/3.9.6-GCCcore-11.2.0 #do I need to do this with some packages/modules? install pingouin
@@ -15,7 +17,6 @@ echo "------------------------------------------------"
 
 output_path = /well/margulies/projects/clinical_grads/Results
 
-for subject in cat SubjectsCompleteData.txt;do
+for subject in 'cat SubjectsCompleteData.txt';do
     python3 -u hcp_class_alignment.py $subject $output_path
 done
-
