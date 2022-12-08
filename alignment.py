b558c442347ee38b09372881f1be2d119a364207
Victoria Shevchenko <victoria.shevchenko@ens.psl.eu>
2022-12-08 17:28:45 +0100

    hopefully last fix

--- alignment.py
+++ alignment.py
@@ -7,22 +7,29 @@ import sys
 from brainspace.gradient.alignment import ProcrustesAlignment
 
 subj = sys.argv[1]
+#subj = "100206"
 odir = sys.argv[2]
 kernel = 4
         
 clusterPath='/well/margulies/projects/data/hcpGrads'
 
 margulies_grads = np.load(f'{clusterPath}/margulies_grads_32k.npy') # 3 grads margulies 2016
+#margulies_grads = np.load('margulies_grads_32K.npy') # 3 grads margulies 2016
+
 alignment = ProcrustesAlignment(verbose=True)
 
 gradses1=np.load(f'{clusterPath}/{subj}/{subj}.mapalign.ses1.diffmap.s0{kernel}mm.npy')
 gradses2=np.load(f'{clusterPath}/{subj}/{subj}.mapalign.ses2.s0{kernel}mm.diffmap.npy')
 
-AllGrads = np.stack((gradses1, gradses2))
-print(AllGrads.shape)
-print(margulies_grads.shape)
+#gradses1=np.load(f'HCP/Gradients/{subj}/{subj}.mapalign.ses1.diffmap.s0{kernel}mm.npy')
+#gradses2=np.load(f'HCP/Gradients/{subj}/{subj}.mapalign.ses2.s0{kernel}mm.diffmap.npy')
 
-allGradsAlignedObject = alignment.fit(AllGrads, margulies_grads) # aligning to margulies 2016
+AllGrads = np.stack((gradses1, gradses2)).reshape(2, 59412, 3)
+#print(margulies_grads.shape)
+ 
+allGradsAlignedObject = alignment.fit(AllGrads, margulies_grads.T) # aligning to margulies 2016
 allGradsAligned = allGradsAlignedObject.aligned_
 
 np.save(arr = allGradsAligned, file =f"{clusterPath}/{subj}.GradsAligned2Margulies2016.npy")
+#np.save(arr = allGradsAligned, file ="GradsAligned2Margulies2016.npy")
+
