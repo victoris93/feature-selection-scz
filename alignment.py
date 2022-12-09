a5cf935b35c467aa80e7a13c2f415c861f2a64e7
Victoria Shevchenko <victoria.shevchenko@ens.psl.eu>
2022-12-09 14:34:32 +0100

    very last fix

--- alignment.py
+++ alignment.py
@@ -16,18 +16,15 @@ clusterPath='/well/margulies/projects/data/hcpGrads'
 margulies_grads = np.load(f'{clusterPath}/margulies_grads_32k.npy') # 3 grads margulies 2016
 #margulies_grads = np.load('margulies_grads_32K.npy') # 3 grads margulies 2016
 
-alignment = ProcrustesAlignment(verbose=True)
+alignment = ProcrustesAlignment(verbose=True, n_iter = 10)
 
-gradses1=np.load(f'{clusterPath}/{subj}/{subj}.mapalign.ses1.diffmap.s0{kernel}mm.npy')
-gradses2=np.load(f'{clusterPath}/{subj}/{subj}.mapalign.ses2.s0{kernel}mm.diffmap.npy')
+gradses1=np.load(f'{clusterPath}/{subj}/{subj}.mapalign.ses1.diffmap.s0{kernel}mm.npy').T
+gradses2=np.load(f'{clusterPath}/{subj}/{subj}.mapalign.ses2.s0{kernel}mm.diffmap.npy').T
 
-#gradses1=np.load(f'HCP/Gradients/{subj}/{subj}.mapalign.ses1.diffmap.s0{kernel}mm.npy')
-#gradses2=np.load(f'HCP/Gradients/{subj}/{subj}.mapalign.ses2.s0{kernel}mm.diffmap.npy')
+#gradses1=np.load(f'HCP/Gradients/{subj}/{subj}.mapalign.ses1.diffmap.s0{kernel}mm.npy').T
+#gradses2=np.load(f'HCP/Gradients/{subj}/{subj}.mapalign.ses2.s0{kernel}mm.diffmap.npy').T
 
-AllGrads = np.stack((gradses1, gradses2)).reshape(2, 59412, 3)
-#print(margulies_grads.shape)
- 
-allGradsAlignedObject = alignment.fit(AllGrads, margulies_grads.T) # aligning to margulies 2016
+allGradsAlignedObject = alignment.fit([gradses1, gradses2], margulies_grads.T) # aligning to margulies 2016
 allGradsAligned = allGradsAlignedObject.aligned_
 
 np.save(arr = allGradsAligned, file =f"{clusterPath}/{subj}.GradsAligned2Margulies2016.npy")
