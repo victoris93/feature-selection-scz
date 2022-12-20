870cb64b82424ec9e78458e7d547b2aff4dfe966
Victoria Shevchenko <victoria.shevchenko@ens.psl.eu>
2022-12-20 08:30:44 +0100

    fixes for the cluster

--- icc_job.sh
+++ icc_job.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 #$ -wd /gpfs3/well/margulies/users/anw410/Vic
-#$ -N DespReliabilityICC
+#$ -N DispVertexICC
 #$ -j y
 #$ -o /gpfs3/well/margulies/users/anw410/Vic/log
 #$ -q short.qc
-#$ -pe shmem 6
-#$ -t 1:9
-
-NEIGHBOURS_LIST=/gpfs3/well/margulies/users/anw410/Vic/n_neighbours.txt
+#$ -pe shmem 4
+#$ -t 1-60
 
 module load Python/3.9.6-GCCcore-11.2.0
 source /well/margulies/users/mnk884/Victoria/ClinicalGradients-skl/bin/activate
 
-FILENAME=$(sed -n "${SGE_TASK_ID}p" $NEIGHBOURS_LIST)
-
-echo "------------------------------------------------"
-echo "Run on host: "`hostname`
-echo "Operating system: "`uname -s`
-echo "Username: "`whoami`
-echo "Started at: "`date`
-echo "------------------------------------------------"
-
-python3 -u vertex_ICC_32k.py $FILENAME
+python -u vertex_ICC_32k.py ${SGE_TASK_ID} $1
--- vertex_ICC_32k.py
+++ vertex_ICC_32k.py
@@ -1,14 +1,34 @@
-from icc_utils import vertexICC
 import sys
 import numpy as np 
 import pandas as pd
 import os
+from pingouin import intraclass_corr
 
-n_neighbours = sys.argv[1]
-
+#N_Neighbours = [50, 100, 200, 400, 800, 1600, 3200, 6400, 12800]
+#n_neighbours = N_Neighbours[int(sys.argv[1]) - 1]
 odir = r'/gpfs3/well/margulies/users/anw410/Vic/Results'
 
-### ICC analysis ###
-Disp_df = pd.read_csv(f"{odir}/GradDispersionDf_{n_neighbours}Neighbours.csv")
-vertexICC(Disp_df, ICC_type = "ICC2", save = True, filename = f"{odir}/{n_neighbours}_Neighbours_DispVertexICC.csv")
+inp = sys.argv[1]
+n_neighbours = sys.argv[2]
+
+if inp < 60:
+    id_s = (inp-1)*1000
+    id_e = inp * 1000 
+
+else:
+    id_s = (inp-1)*1000
+    id_e = id_s + 412
+
+icc_arr = np.empty((1000))
+
+for i in range(id_s, id_e):
+    ### ICC analysis ###
+    Disp_df = pd.read_csv(f"{odir}/GradDispersionDf_{n_neighbours}Neighbours.csv")
+    #Disp_df = pd.melt(Disp_df, id_vars = ("Vertex", "Session"), var_name = "Subject", value_name = "Dispersion")
+    f = Disp_df.loc[[i,i + 59412]]
+    f = f.drop(["Unnamed: 0","Vertex"], axis=1).melt(id_vars = ("Session"), var_name="Subject", value_name = "Dispersion")
+    ICC = intraclass_corr(f, targets = "Subject", raters = "Session", ratings = "Dispersion")
+    icc_arr[i] = ICC[ICC["Type"] == "ICC2"]["ICC"]
+
+np.save(arr = icc_arr, file = f"{odir}/icc_arr_{n_neighbours}_neighbours_{inp}.npy")
 
