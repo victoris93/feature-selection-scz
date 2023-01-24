1ae5bb36ef1cfecb2da4f20b782a160e61dbe0df
Victoria Shevchenko <victoria.shevchenko@ens.psl.eu>
2023-01-24 22:49:33 +0100

    procrustes alignment on the cluster

--- send_job_align.sh
+++ send_job_align.sh
@@ -1,5 +1,4 @@
 #!/bin/bash 
-
 #SBATCH --job-name=GradAlign
 #SBATCH -o ./logs/GradAlign-%j.out
 #SBATCH -p short
@@ -7,19 +6,16 @@
 #SBATCH --cpus-per-task=6
 #SBATCH --requeue
 #SBATCH --array 1-1018:1
+
 SUBJECT_LIST=./SubjectsCompleteData.txt
 
-# I want the job to take 3 gradients of all 900 subjects, align them to Margulies 2016 and compite ICC for each vertex abd value of neighbours. Returns a huge csv with results, a long dataframe
-module load Python/3.9.6-GCCcore-11.2.0 #do I need to do this with some packages/modules? install pingouin
-source /well/margulies/users/mnk884/Victoria/ClinicalGradients-skl/bin/activate
+module load Python/3.9.6-GCCcore-11.2.0
+source /gpfs3/users/margulies/cpy397/env/ClinicalGrads/bin/activate
 
-mkdir -p /well/margulies/projects/clinical_grads/Results
-output_path=/well/margulies/projects/clinical_grads/Results
+output_path=/gpfs3/well/margulies/users/cpy397/AlignedGradsPCA/1000_iter
 echo Executing task ${SLURM_ARRAY_TASK_ID} of job ${SLURM_ARRAY_JOB_ID} on `hostname` as user ${USER} 
 echo the job id is $SLURM_ARRAY_JOB_ID
 FILENAME=$(sed -n "${SLURM_ARRAY_TASK_ID}p" $SUBJECT_LIST)
-#echo echo $SLURM_ARRAY_JOB_ID
-#echo "Processing subject $FILENAME"
 
 echo python3 -u alignment.py $FILENAME  $output_path
 python3 -u alignment.py $FILENAME  $output_path
