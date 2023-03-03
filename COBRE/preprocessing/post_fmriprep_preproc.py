148cf8066332d60f02498995ce092206d1c6b322
Victoria Shevchenko <victoria.shevchenko@ens.psl.eu>
2023-03-03 12:28:04 +0100

    discard first 10 volumes

--- COBRE/preprocessing/post_fmriprep_preproc.py
+++ COBRE/preprocessing/post_fmriprep_preproc.py
@@ -9,12 +9,9 @@ from nilearn import signal
 from sklearn.impute import SimpleImputer
 from nilearn.maskers import NiftiLabelsMasker
 
-# subject = sys.argv[1]
-
-subject = 'A00018979'
+subject = sys.argv[1]
 
 data_path = '/gpfs3/well/margulies/projects/data/COBRE'
-subject_list = np.loadtxt(f'{data_path}/COBRE_subjects.txt', dtype = 'str')
 
 def get_sessions(subject, data = data_path):
     data = data + '/derivatives/fmriprep'
@@ -26,10 +23,13 @@ def get_sessions(subject, data = data_path):
             session_names.append(subdir[4:])
     return session_names
 
-def impute_nans(dataframe, pick_columns = None):
+def impute_nans(dataframe, pick_columns = None, gsr = False):
     imputer = SimpleImputer(strategy='mean')
     if pick_columns is not None and isinstance(pick_columns, (list, np.ndarray)):
-        df_no_nans = pd.DataFrame(imputer.fit_transform(dataframe), columns=dataframe.columns)[pick_columns]
+        if gsr == False:
+            df_no_nans = pd.DataFrame(imputer.fit_transform(dataframe), columns=dataframe.columns)[pick_columns[1:]]
+        else:
+            df_no_nans = pd.DataFrame(imputer.fit_transform(dataframe), columns=dataframe.columns)[pick_columns]
     else:
         df_no_nans = pd.DataFrame(imputer.fit_transform(dataframe), columns=dataframe.columns)
     return df_no_nans
@@ -68,10 +68,10 @@ def parcellate(subject_ts_paths, confounds, parcellation = 'schaefer'):
 def clean_signal(parc_ts_list):
     clean_ts = []
     clean_ts_s1 = signal.clean(parc_ts_list[0], t_r = 2, low_pass=0.08, high_pass=0.01, standardize=True, detrend=True)
-    clean_ts.append(clean_ts_s1)
+    clean_ts.append(clean_ts_s1[10:]) # discarding the first 10 volumes
     if len(parc_ts_list) > 1:
         clean_ts_s2 = signal.clean(parc_ts_list[1], t_r = 2, low_pass=0.08, high_pass=0.01, standardize=True, detrend=True)
-        clean_ts.append(clean_ts_s2)
+        clean_ts.append(clean_ts_s2[10:])
     return clean_ts
 
 session_names = get_sessions(subject)
@@ -95,4 +95,4 @@ output_dir = f'{data_path}/clean_data/sub-{subject}/func'
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-np.save(file = f'{output_dir}/sub-{subject}_task-rest_space-MNI152NLin2009cAsym_res-2_desc-clean_bold.nii.gz', arr = clean_parcellated_ts)
+np.save(file = f'{output_dir}/sub-{subject}_task-rest_space-MNI152NLin2009cAsym_res-2_desc-clean_bold', arr = clean_parcellated_ts)
