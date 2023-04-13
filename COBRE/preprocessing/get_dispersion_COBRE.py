62e48880ab7c08892e73f2270326c6271e4962e5
Victoria Shevchenko <victoria.shevchenko@ens.psl.eu>
2023-04-13 17:44:44 +0200

    in case we want to test each gradient separately

---
+++ COBRE/preprocessing/get_dispersion_COBRE.py
@@ -0,0 +1,29 @@
+import numpy as np
+from sklearn.neighbors import NearestNeighbors
+import sys
+
+n_grads = int(sys.argv[1])
+n_neighbours = int(sys.argv[2])
+
+scz_path = '/well/margulies/projects/data/COBRE/clean_data/SCZ/schaefer1000'
+controls_path = '/well/margulies/projects/data/COBRE/clean_data/controls/schaefer1000'
+
+scz_subjects = np.loadtxt(f'{scz_path}/COBRE_SCZ_subjects.txt', dtype = str)
+control_subjects = np.loadtxt(f'{controls_path}/COBRE_controls_subjects.txt', dtype = str)
+
+n_neighbours_list = np.arange(5, 505, 5)
+n_grads_list = np.arange(1, 11)
+
+for subject in scz_subjects:
+    gradients = np.expand_dims(np.load(f'{scz_path}/sub-{subject}/func/aligned_10grads_{subject}_schaefer1000.npy')[:, n_grads - 1], axis = 1)
+    hcp_ngbrs = NearestNeighbors(n_neighbors=n_neighbours, algorithm='brute').fit(gradients)
+    distances, indices = hcp_ngbrs.kneighbors(gradients)
+    subj_disp = distances.mean(axis = 1)
+    np.save(f'{scz_path}/sub-{subject}/func/disp_{n_grads}_{n_neighbours}n_{subject}_schaefer1000.npy', subj_disp)
+
+for subject in control_subjects:
+    gradients = np.expand_dims(np.load(f'{controls_path}/sub-{subject}/func/aligned_10grads_{subject}_schaefer1000.npy')[:, n_grads - 1], axis = 1)
+    hcp_ngbrs = NearestNeighbors(n_neighbors=n_neighbours, algorithm='brute').fit(gradients)
+    distances, indices = hcp_ngbrs.kneighbors(gradients)
+    subj_disp = distances.mean(axis = 1)
+    np.save(f'{controls_path}/sub-{subject}/func/disp_{n_grads}_{n_neighbours}n_{subject}_schaefer1000.npy', subj_disp)
