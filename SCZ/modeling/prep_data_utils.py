95476555fd9e4813138f1fd932c8194a1782ea16
Victoria Shevchenko <victoria.shevchenko@ens.psl.eu>
2023-07-08 10:18:31 +0200

    added nbs thresholding

--- SCZ/modeling/prep_data_utils.py
+++ SCZ/modeling/prep_data_utils.py
@@ -30,7 +30,7 @@ def threshMat(conn,lim): # if 5th percentile, then lim=95
 		conn[i, conn[i,:] < perc[i]] = 0   
 	return conn
 
-def prepare_data_csv(data_paths, diag_mapping = None):
+def prepare_data_csv(data_paths, diag_mapping = diagnosis_mapping):
     data_csv = []
     for dataset in list(data_paths.keys()):
         participants = pd.read_csv(f'{data_paths[dataset]}/participants.tsv', sep='\t')[["participant_id", "diagnosis"]]
@@ -44,16 +44,16 @@ def prepare_data_csv(data_paths, diag_mapping = None):
     data_csv["participant_id"] = data_csv["participant_id"].str.replace('sub-', '')
     return data_csv
 
-def load_data(data_csv, data_type, comb_grads = None, n_grad = None, n_neighbours = None, aligned_grads = True, feat_selection = None, percentile = None):
+def load_data(data_csv, data_type, comb_grads = None, n_grad = None, n_neighbours = None, aligned_grads = True, feat_selection = None, percentile = None, nbs_thresh = None, nbs_dir = None):
     '''
-    data_type: 'conn', 'disp', 'grad'
+    data_type: 'conn', 'disp', 'grad', 'nbs'
     '''
 
     data = []
     # add progress bar
     if feat_selection is not None:
-        if percentile is None:
-            raise ValueError("Percentile must be specified for feature selection.")
+        if percentile is None and nbs_thresh is None:
+            raise ValueError("Either percentile or nbs_thresh must be specified for feature selection.")
                 
     for subject in tqdm.tqdm(data_csv['participant_id']):
         root_path = data_csv[data_csv['participant_id'] == subject]['path'].values[0]
@@ -72,16 +72,25 @@ def load_data(data_csv, data_type, comb_grads = None, n_grad = None, n_neighbour
         try:
             features = [np.load(f'{subj_path}/{i}') for i in os.listdir(subj_path) if data_type in i and aligned in i][0]
             if data_type == 'conn':
-                if len(features.shape) == 3:
+                while len(features.shape) > 2:
                     features = features[0]
                 np.fill_diagonal(features, 0)
                 features = sym_matrix_to_vec(features, discard_diagonal=True)
+                if feat_selection == 'nbs':
+                    nbs_thresh = float(nbs_thresh)
+                    if nbs_dir is None:
+                        nbs_dir = os.getcwd()
+                    adj = np.load(f'{nbs_dir}/nbs_{nbs_thresh}thresh.npy', allow_pickle=True)[1]
+                    adj = sym_matrix_to_vec(adj, discard_diagonal=True)
+                    features = features[adj != 0]
 
             elif data_type == 'gradients':
                 if comb_grads:
                     features = features[0,:, :n_grad].ravel()
                 elif not comb_grads:
                     features = features[0,:, n_grad - 1]
+            elif data_type == 'nbs':
+                adj = features
             data.append(features)
 
         except FileNotFoundError as e:
@@ -100,51 +109,3 @@ def load_data(data_csv, data_type, comb_grads = None, n_grad = None, n_neighbour
     data["diagnosis"] = data_csv['diagnosis'].values
     #data = data.dropna()
     return data, data_csv
-
-
-def load_data_parallel(data_csv, data_type, comb_grads = None, n_grad = None, n_neighbours = None, aligned_grads = True):
-    '''
-    data_type: 'conn', 'disp', 'grad'
-    '''
-    data = []
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = []
-        for subject in data_csv['participant_id']:
-            futures.append(executor.submit(load_data_worker, subject, data_csv, data_type, comb_grads, n_grad, n_neighbours, aligned_grads))
-        for future in concurrent.futures.as_completed(futures):
-            data.append(future.result())
-    data = pd.concat(data, ignore_index=True, axis=0)
-    data = pd.merge(data, data_csv, on='participant_id')
-    return data, data_csv
-
-def load_data_worker(subject, data_csv, data_type, comb_grads, n_grad, n_neighbours, aligned_grads):
-    root_path = data_csv[data_csv['participant_id'] == subject]['path'].values[0]
-    subj_path = f'{root_path}/sub-{subject}/func'
-    aligned = ''
-    if data_type == 'grad':
-        data_type = 'gradients'
-        if aligned_grads:
-            aligned = 'aligned'
-    if data_type == 'disp' and comb_grads:
-        data_type = f'disp-comb-{n_grad}grad-{n_neighbours}n'
-    elif data_type == 'disp' and not comb_grads:
-        data_type = f'disp-sing-{n_grad}grad-{n_neighbours}n'
-
-    try:
-        features = [np.load(f'{subj_path}/{i}') for i in os.listdir(subj_path) if data_type in i and aligned in i][0]
-        if data_type == 'conn':
-            if len(features.shape) == 3:
-                features = features[0]
-            features = sym_matrix_to_vec(features, discard_diagonal=True)
-        elif data_type == 'gradients':
-            if comb_grads:
-                features = features[0,:, :n_grad].ravel()
-            elif not comb_grads:
-                features = features[0,:, n_grad - 1]
-        features = pd.DataFrame(features).T
-        features["participant_id"] = subject
-        return features
-
-    except FileNotFoundError as e:
-        print(f"Data not found for subject {subject}: {e}.")
-        data_csv = data_csv[data_csv['participant_id'] != subject]
