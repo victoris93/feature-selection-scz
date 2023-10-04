c431bf40c160475d563b43e0a4a564177d6450f7
Victoria Shevchenko <victoria.shevchenko@ens.psl.eu>
2023-10-04 16:05:02 +0200

    selection of the best model based on cv

--- SCZ/modeling/fit_best_features.py
+++ SCZ/modeling/fit_best_features.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 
 n_features = int(sys.argv[1])
-cv = sys.argv[2] # either "cv" or "test"
+# cv = sys.argv[2] # either "cv" or "test"
 
 
 best_features = pd.read_csv(f"best_features/{n_features}_best_features.csv")
@@ -13,22 +13,20 @@ print("Features loaded...")
 diagnosis = pd.read_csv("participants.csv")['diagnosis'].values
 best_features["diagnosis"] = diagnosis
 
-if cv == "cv":
-    performance = fit_on_best_features(best_features, cv = True)
-else:
-    performance = fit_on_best_features(best_features, cv = False)
 
-print(f"Models fitted for {n_features} features.")
-performance.drop(columns = ["Recall", "Prec.", "Kappa", "MCC", "TT (Sec)"])
+performance = fit_on_best_features(best_features)
+
+
+print(f"CV done for all models for {n_features} features.")
 performance["N features"] = n_features
-print(performance)
 
-if os.path.exists(f"results/model_results_{cv}.csv"):
-    model_results = pd.read_csv(f"results/model_results_{cv}.csv")
+
+if os.path.exists(f"results/model_results_cv.csv"):
+    model_results = pd.read_csv(f"results/model_results_cv.csv")
     model_results = pd.concat([model_results, performance], axis = 0)
-    model_results.to_csv(f"results/model_results_{cv}.csv", index = False)
+    model_results.to_csv(f"results/model_results_cv.csv", index = False)
 else:
-    performance.to_csv(f"results/model_results_{cv}.csv", index = False)
+    performance.to_csv(f"results/model_results_cv.csv", index = False)
 
 
 
