dd1aba099ae404568897e458bb2711e68574baf7
Victoria Shevchenko <victoria.shevchenko@ens.psl.eu>
2024-01-03 17:18:45 +0100

    job scripts

--- SCZ/modeling/job_best_936_grad.sh
+++ SCZ/modeling/job_best_936_grad.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
-#SBATCH --job-name=BestConn
-#SBATCH -o ./logs/BestConn-%j.out
+#SBATCH --job-name=Best936Grad
+#SBATCH -o ./logs/Best936Grad-%j.out
 #SBATCH -p short
 #SBATCH --constraint="skl-compat"
 #SBATCH --cpus-per-task=5
@@ -8,7 +8,6 @@
 module load Python/3.9.6-GCCcore-11.2.0
 source /well/margulies/users/cpy397/env/ClinicalGrads/bin/activate
 
-echo the task id is $SLURM_ARRAY_JOB_ID
 echo the job id is $SLURM_JOB_ID
 
-python3 -u fit_best_936_conn.py
+python3 -u fit_best_936_grad.py
